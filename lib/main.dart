e96333ac1e27bfe5909b1f241c513fca0b7cdb45
Arima1213 <arima3992@gmail.com>
2024-06-08 13:29:08 +0700

    Refactor main function and add MyApp class in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return const MaterialApp(
+      home: Home(),
     );
   }
 }
